173ed13f95cd2f40cd699b50917a418ae5a0676c
adekolacode <kolade2127@gmail.com>
2020-02-19 16:37:54 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,357 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+  <title>Learnpro</title>
+  <meta content="" name="descriptison">
+  <meta content="" name="keywords">
+
+  <!-- Favicons -->
+  <link href="" rel="icon">
+  <link href="" rel="apple-touch-icon">
+
+  <!-- Google Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,600,600i,700,700i" rel="stylesheet">
+
+  
+  <link href="css/bootstrap.min.css" rel="stylesheet">
+  <link rel="stylesheet" href="fonts/css/all.min.css">
+  <link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+<header>
+  <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-success">
+    <a class="navbar-brand" href="#">Learnpro</a>
+    <button class="navbar-toggler collapsed" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="navbar-collapse collapse" id="navbarCollapse" style="">
+      <ul class="navbar-nav ml-auto">
+        <li class="nav-item">
+          <a class="nav-link" href="#">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">About Us</a>
+        </li>
+        <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Categories
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+          <a class="dropdown-item" href="#">Action</a>
+          <a class="dropdown-item" href="#">Another action</a>
+          <a class="dropdown-item" href="#">Something else here</a>
+          <a class="dropdown-item" href="#">Action</a>
+          <a class="dropdown-item" href="#">Another action</a>
+          <a class="dropdown-item" href="#">Something else here</a>
+          <a class="dropdown-item" href="#">Action</a>
+          <a class="dropdown-item" href="#">Another action</a>
+          <a class="dropdown-item" href="#">Something else here</a>
+          <a class="dropdown-item" href="#">Action</a>
+          <a class="dropdown-item" href="#">Another action</a>
+          <a class="dropdown-item" href="#">Something else here</a>
+          <a class="dropdown-item" href="#">Action</a>
+          <a class="dropdown-item" href="#">Another action</a>
+          <a class="dropdown-item" href="#">Something else here</a>
+        </div>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Register</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Login</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Contact Us</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Forum</a>
+        </li>
+      </ul>
+    </div>
+  </nav>
+</header>
+<!--Start of content-->
+<main role="main">
+  <section class="jumbotron text-center mt-4">
+    <div class="container">
+      <h2>You are welcome!!!</h2>
+      <p class="lead text-muted">Get yourself prepared for future by learning top demanding skills from professionals</p>
+      <p>
+        <a href="#" class="btn btn-sm btn-success my-2">Teach on learnpro</a>
+        <a href="#" class="btn btn-sm btn-success my-2">Freelance</a>
+        <a href="#" class="btn btn-sm btn-success my-2">Jobs</a>
+      </p>
+    </div>
+  </section>
+
+  <div class="py-5 bg-light">
+      
+      <div class="container">
+      <div class="row mb-4 justify-content-center">
+          <div class="col-md-6">
+            <form action="">
+              <div class="input-group">
+                <input type="text" name="q" class="form-control" placeholder="Search for anything">
+                <button class="btn btn-success"><i class="fa fa-cog"></i></button>
+              </div>
+            </form>
+          </div>
+      </div>
+      <div class="row">
+        <div class="col-lg-4 col-md-6">
+          <div class="card mb-4 shadow-sm">
+          <img src="http://placehold.it/300" alt="..." class="img-thumbnail img-fluid card-img-top" style="height:225px;">
+            <div class="card-body">
+              <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+              <div class="d-flex justify-content-between align-items-center">
+                <div class="btn-group">
+                  <button type="button" class="btn btn-sm btn-outline-success">preview</button>
+                  <button type="button" class="btn btn-sm btn-outline-success">Enroll</button>
+                </div>
+                <span class="text-muted">10:00 mins</span>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col-lg-4 col-md-6">
+          <div class="card mb-4 shadow-sm">
+          <img src="http://placehold.it/300" alt="..." class="img-thumbnail img-fluid card-img-top" style="height:225px;">
+            <div class="card-body">
+              <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+              <div class="d-flex justify-content-between align-items-center">
+                <div class="btn-group">
+                  <button type="button" class="btn btn-sm btn-outline-success">preview</button>
+                  <button type="button" class="btn btn-sm btn-outline-success">Enroll</button>
+                </div>
+                <span class="text-muted">10:00 mins</span>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col-lg-4 col-md-6">
+          <div class="card mb-4 shadow-sm">
+          <img src="http://placehold.it/300" alt="..." class="img-thumbnail img-fluid card-img-top" style="height:225px;">
+            <div class="card-body">
+              <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+              <div class="d-flex justify-content-between align-items-center">
+                <div class="btn-group">
+                  <button type="button" class="btn btn-sm btn-outline-success">preview</button>
+                  <button type="button" class="btn btn-sm btn-outline-success">Enroll</button>
+                </div>
+                <span class="text-muted">10:00 mins</span>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="col-lg-4 col-md-6">
+          <div class="card mb-4 shadow-sm">
+          <img src="http://placehold.it/300" alt="..." class="img-thumbnail img-fluid card-img-top" style="height:225px;">
+            <div class="card-body">
+              <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+              <div class="d-flex justify-content-between align-items-center">
+                <div class="btn-group">
+                  <button type="button" class="btn btn-sm btn-outline-success">preview</button>
+                  <button type="button" class="btn btn-sm btn-outline-success">Enroll</button>
+                </div>
+                <span class="text-muted">10:00 mins</span>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col-lg-4 col-md-6">
+          <div class="card mb-4 shadow-sm">
+          <img src="http://placehold.it/300" alt="..." class="img-thumbnail img-fluid card-img-top" style="height:225px;">
+            <div class="card-body">
+              <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+              <div class="d-flex justify-content-between align-items-center">
+                <div class="btn-group">
+                  <button type="button" class="btn btn-sm btn-outline-success">preview</button>
+                  <button type="button" class="btn btn-sm btn-outline-success">Enroll</button>
+                </div>
+                <span class="text-muted">10:00 mins</span>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col-lg-4 col-md-6">
+          <div class="card mb-4 shadow-sm">
+          <img src="http://placehold.it/300" alt="..." class="img-thumbnail img-fluid card-img-top" style="height:225px;">
+            <div class="card-body">
+              <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+              <div class="d-flex justify-content-between align-items-center">
+                <div class="btn-group">
+                  <button type="button" class="btn btn-sm btn-outline-success">preview</button>
+                  <button type="button" class="btn btn-sm btn-outline-success">Enroll</button>
+                </div>
+                <span class="text-muted">10:00 mins</span>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="col-lg-4 col-md-6">
+          <div class="card mb-4 shadow-sm">
+          <img src="http://placehold.it/300" alt="..." class="img-thumbnail img-fluid card-img-top" style="height:225px;">
+            <div class="card-body">
+              <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+              <div class="d-flex justify-content-between align-items-center">
+                <div class="btn-group">
+                  <button type="button" class="btn btn-sm btn-outline-success">preview</button>
+                  <button type="button" class="btn btn-sm btn-outline-success">Enroll</button>
+                </div>
+                <span class="text-muted">10:00 mins</span>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col-lg-4 col-md-6">
+          <div class="card mb-4 shadow-sm">
+          <img src="http://placehold.it/300" alt="..." class="img-thumbnail img-fluid card-img-top" style="height:225px;">
+            <div class="card-body">
+              <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+              <div class="d-flex justify-content-between align-items-center">
+                <div class="btn-group">
+                  <button type="button" class="btn btn-sm btn-outline-success">preview</button>
+                  <button type="button" class="btn btn-sm btn-outline-success">Enroll</button>
+                </div>
+                <span class="text-muted">10:00 mins</span>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col-lg-4 col-md-6">
+          <div class="card mb-4 shadow-sm">
+          <img src="http://placehold.it/300" alt="..." class="img-thumbnail img-fluid card-img-top" style="height:225px;">
+            <div class="card-body">
+              <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+              <div class="d-flex justify-content-between align-items-center">
+                <div class="btn-group">
+                  <button type="button" class="btn btn-sm btn-outline-success">preview</button>
+                  <button type="button" class="btn btn-sm btn-outline-success">Enroll</button>
+                </div>
+                <span class="text-muted">10:00 mins</span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!--start of pagination-->
+  <ul class="pagination justify-content-center mt-2">
+    <li class="page-item">
+      <a class="page-link" href="#">&larr; Older</a>
+    </li>
+    <li class="page-item">
+      <a class="page-link" href="#">Newer &rarr;</a>
+    </li>
+  </ul>
+<!--End of pagination-->
+  </div>
+
+</main>
+<!--End of content-->
+
+<!--start of footer-->	
+<footer class="footer">		
+		<div class="container-fluid">
+			<div class="row">
+				<div class="col-md-3 m-b-30">
+					<div class="footer-title m-t-5 m-b-20 p-b-8">
+						About us
+					</div>	
+					<p class="white-text">
+						Our aim is to equip youths in Nigeria with top demanding skills which will help in reducing the rate of unemployment in Nigeria
+					</p>
+				</div>
+				<div class="col-md-3 m-b-30">
+					<div class="footer-title m-t-5 m-b-20 p-b-8">
+						Latest Courses
+					</div>	
+					<div class="footer-links">
+						<a href="#">
+							Word Excel
+						</a>
+						<a href="#">
+							Mini Importation
+						</a>
+						<a href="#">
+							Graphics design
+						</a>
+						<a href="#">
+							Web desingn in ten minutes
+						</a>
+					</div>
+				</div>
+				<div class="col-md-3 m-b-30">
+					<div class="footer-title m-t-5 m-b-20 p-b-8">
+						Quick Links
+					</div>	
+					<div class="footer-links">
+						<a href="#">
+							Blog
+						</a>
+						<a href="#">
+							FAQ
+						</a>
+						<a href="#">
+							Terms & conditions
+						</a>
+						<a href="#">
+							Privacy policy
+						</a>
+					</div>
+				</div>
+				<div class="col-md-3 m-b-30">
+					<div class="footer-title m-t-5 m-b-20 p-b-8">
+						Support
+					</div>	
+					<div class="footer-links">
+						<a href="#">
+							Login
+						</a>
+						<a href="#">
+							Sign up
+						</a>
+						<a href="#">
+							Support forum
+						</a>
+					</div>
+
+					<div class="footer-social-links m-t-30">
+						<li>
+							<a href="#">
+              <i class="fab fa-facebook"></i>
+							</a>
+							<a href="#">
+                <i class="fab fa-twitter"></i>
+							</a>
+							<a href="#">
+                <i class="fab fa-linkedin"></i>
+							</a>
+							<a href="#">
+                <i class="fab fa-youtube"></i>
+							</a>
+						</li>
+					</div>
+				</div>
+			</div>
+		</div>
+	</footer>
+	<div class="footer-bottom">
+		Copyright &copy; 2020 All rights reserved
+	</div>
+<!--End of footer-->
+
+  <script src="js/jquery.min.js"></script>
+  <script src="js/bootstrap.min.js"></script>
+  <script src="js/popper.min.js"></script>
+  <script src="js/main.js"></script>
+</body>
+
+</html>
